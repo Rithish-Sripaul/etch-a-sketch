65b9c3e8f8b8e0463f72c15824cbd1498d7915ed
Rithish-Sripaul <rithishsripaul@gmail.com>
2022-09-19 22:16:22 +0530

    Drawing function to the canvas
    The user can now draw on the canvas by hovering over it. They can also edit the number of pixels in the JS file. The color is black.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const canvas = document.querySelector(".canvas");
+
+const pixelCount = 16;
+const num = 620 / pixelCount;
+
+for (let i = 0; i < pixelCount; i++) {
+  const pixelRow = document.createElement("div");
+  pixelRow.classList.add("pixel-row");
+  for (let j = 0; j < pixelCount; j++) {
+    const p = document.createElement("div");
+    p.style.minWidth = num.toString() + "px";
+    p.style.height = num.toString() + "px";
+    p.classList.add("pixel");
+    pixelRow.appendChild(p);
+  }
+  canvas.appendChild(pixelRow);
+}
+
+const pixels = document.querySelectorAll(".pixel");
+
+pixels.forEach((pixel) => {
+  pixel.addEventListener("mouseover", () => {
+    pixel.style.backgroundColor = "#000";
+  });
+});
