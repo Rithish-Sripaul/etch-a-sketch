1321cf629a5095620ccb65af1d4a8b2592c29e96
Rithish-Sripaul <rithishsripaul@gmail.com>
2022-09-19 22:40:19 +0530

    Custom Sized Canvas
    We can now choose the size of the canvas ourselvs via a Input fied and a button.

--- script.js
+++ script.js
@@ -2,26 +2,35 @@
 
 const canvas = document.querySelector(".canvas");
 
-const pixelCount = 16;
-const num = 620 / pixelCount;
-
-for (let i = 0; i < pixelCount; i++) {
-  const pixelRow = document.createElement("div");
-  pixelRow.classList.add("pixel-row");
-  for (let j = 0; j < pixelCount; j++) {
-    const p = document.createElement("div");
-    p.style.minWidth = num.toString() + "px";
-    p.style.height = num.toString() + "px";
-    p.classList.add("pixel");
-    pixelRow.appendChild(p);
+function createCnavas(pixelCount, num) {
+  // Removing the child elements of the old Canvas
+  while (canvas.childElementCount > 0) {
+    canvas.removeChild(canvas.firstElementChild);
   }
-  canvas.appendChild(pixelRow);
+  for (let i = 0; i < pixelCount; i++) {
+    const pixelRow = document.createElement("div");
+    pixelRow.classList.add("pixel-row");
+    for (let j = 0; j < pixelCount; j++) {
+      const p = document.createElement("div");
+      p.style.minWidth = num.toString() + "px";
+      p.style.height = num.toString() + "px";
+      p.classList.add("pixel");
+      pixelRow.appendChild(p);
+    }
+    canvas.appendChild(pixelRow);
+  }
+
+  const pixels = document.querySelectorAll(".pixel");
+
+  pixels.forEach((pixel) => {
+    pixel.addEventListener("mouseover", () => {
+      pixel.style.backgroundColor = "#000";
+    });
+  });
 }
 
-const pixels = document.querySelectorAll(".pixel");
-
-pixels.forEach((pixel) => {
-  pixel.addEventListener("mouseover", () => {
-    pixel.style.backgroundColor = "#000";
-  });
+const button = document.querySelector("button");
+const pixelCount = document.querySelector("input");
+button.addEventListener("click", () => {
+  createCnavas(parseInt(pixelCount.value), 620 / pixelCount.value);
 });
